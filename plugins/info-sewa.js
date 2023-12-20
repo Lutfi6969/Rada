87a9df4c71810fbd05bac8c6ae4e8615c4a40737
Lutfi6969 <154408155+Lutfi6969@users.noreply.github.com>
2023-12-21 01:35:49 +0700

    Update info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command }) => {
-    let buffer = await fetch(`https://telegra.ph/file/1c53a85e78e81e2aaad9c.jpg`).then(res => res.buffer())
+    let buffer = await fetch(`https://telegra.ph/file/f82a7e100758c82e3a123.png`).then(res => res.buffer())
     conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Jika telah melakukan pembayaran silahkan kirimkan bukti pembayaran ke WhatsApp Owner.*`, m)
 }
 
